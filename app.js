1b8e095616b1ed83431005be8b39a53c800940d2
Jeff Majek <jeffmajek@Administrators-MacBook-Pro.local>
2015-12-15 12:13:31 -0500

    Adding the JS interactions

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
-window.onkeyup = function(e) {
-   var key = e.keyCode ? e.keyCode : e.which;
-   var character = getCharacter(key);
+var keysArray = ["b", "c", "v", "q", "m", "n", "b"];
+var currentKeyIndex = 0;
+var isPlaying = false;
+var isRotatingBody = false;
 
-	switch (new Date().getDay()) {
-    	case 'a':
-    	case 's':
-    	case 'd':
-    	case 'f':
-    	case 'j':
-    	case 'k':
-    	case 'l':
-        	//Blue Buttons
-        	break;
-	    case 'c':
-	    case 'v':
-	    case 'b':
-	    case 'n':
-	    case 'm':
-	        day = "Monday";
-	        break;
-	    case 2:
-	        day = "Tuesday";
-	        break;
-	    case 3:
-	        day = "Wednesday";
-	        break;
-	    case 4:
-	        day = "Thursday";
-	        break;
-	    case 5:
-	        day = "Friday";
-	        break;
-	    case 6:
-	        day = "Saturday";
-	        break;
+
+window.onload = function(e) {
+	if(keysArray) {
+		isPlaying = true;
+		playKeysArray();
+	}
+}
+
+var playKeysArray = function() {
+	var character = keysArray[currentKeyIndex];
+
+	if(currentKeyIndex < keysArray.length) {
+		performAction(character);
+		currentKeyIndex += 1;
+
+		setTimeout(playKeysArray, 800);
+	}
+
+	if (currentKeyIndex == keysArray.length) {
+		keysArray = [];
+		isPlaying = false;
+
+		if(isRotatingBody) {
+			moveBodies();
+		}
+	}
+}
+
+window.onkeydown = function(e) { 
+  return !(e.keyCode == 32);
+};
+
+window.onkeyup = function(e) {
+
+	if(!isPlaying) {
+		var key = e.keyCode ? e.keyCode : e.which;
+	   	var character = getCharacter(key);
+	   	
+	   	keysArray.push(character);
+	   	performAction(character);
 	}
 }
 
@@ -42,14 +51,91 @@ function getCharacter(key) {
 	return letter.toLowerCase();
 }
 
+function getAudioTag() {
+	var audioTag = $('audio');
+	return audioTag[0];
+}
 
-$(document).ready(function(){
+function rotateHead(person) {
+	$('.character.'+ person +' .head .top').addClass("head-rocking").delay(1000).queue(function(){
+    	$(this).removeClass("head-rocking").dequeue();
+	});
+}
 
-	//Grab alll the values $('')
-	var hasIntereactedWith = false;
-	var someElement = $('element');
-	someElement.onclick = function() {
-		var crossWordItem = $(this).data('crowssword-item');
-		hasIntereactedWith true;
+function moveBodies() {
+	if(isRotatingBody) {
+		$('.character').removeClass('character-rocking');
+		isRotatingBody = false;
+   } else {
+   		$('.character').addClass('character-rocking')
+   		isRotatingBody = true;
+   }
+}
+
+function performAction(character) {
+	audioTag = getAudioTag();
+	var validCharacter = true;
+
+	switch (character) {
+    	case 'a':
+    		audioTag.src = 'audio/church_bell_A.mp3';
+    		break;
+    	case 's':
+    		audioTag.src = 'audio/church_bell_B.mp3';
+    		break;
+    	case 'd':
+    		audioTag.src = 'audio/church_bell_C.mp3';
+    		break;
+    	case 'f':
+    		audioTag.src = 'audio/church_bell_D.mp3';
+    		break;
+    	case 'j':
+    		audioTag.src = 'audio/church_bell_E.mp3';
+    		break;
+    	case 'k':
+    		audioTag.src = 'audio/church_bell_F.mp3';
+    		break;
+    	case 'l':
+        	audioTag.src = 'audio/church_bell_G.mp3';
+    		break;
+	    case 'c':
+	    	audioTag.src = 'audio/Vocal-1.mp3';
+	    	rotateHead('mike');
+    		break;
+	    case 'v':
+	    	audioTag.src = 'audio/Vocal-2.mp3';
+	    	rotateHead('jeff');
+    		break;
+	    case 'b':
+	    	audioTag.src = 'audio/Vocal-3.mp3';
+	    	rotateHead('amelia');
+    		break;
+	    case 'n':
+	    	audioTag.src = 'audio/Vocal-4.mp3';
+	    	rotateHead('stacey');
+    		break;
+	    case 'm':
+	        audioTag.src = 'audio/Vocal-5.mp3';
+	        rotateHead('christina');
+    		break;
+    	case 'q':
+	        moveBodies();
+    		break;
+    	case ' ':
+    		audioTag.src = 'audio/Vocal-6.mp3';
+    		rotateHead('mike');
+    		rotateHead('jeff');
+    		rotateHead('amelia');
+    		rotateHead('stacey');
+	        rotateHead('christina');
+    		break;
+    	default:
+    		validCharacter = false;
+    		break;
 	}
-});
+
+	if(validCharacter) {
+		audioTag.load();
+		audioTag.play();
+	}
+}
